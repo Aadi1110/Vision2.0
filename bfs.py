d19200835ca0cea6676bb090e688f2d46a2390b1
Aadi1110 <60649618+Aadi1110@users.noreply.github.com>
2020-11-05 08:31:59 +0530

    Update bfs.py

--- bfs.py
+++ bfs.py
@@ -45,39 +45,27 @@ g.addEdge(43,42)
 g.addEdge(67,76)
 g.addEdge(67,58)
 
-# finds shortest path between 2 nodes of a graph using BFS
 def bfs(graph, start, goal):
-    # keep track of explored nodes
     explored = []
-    # keep track of all the paths to be checked
     queue = [[start]]
  
-    # return path if start is goal
     if start == goal:
         return [start]
  
-    # keeps looping until all possible paths have been checked
     while queue:
-        # pop the first path from the queue
         path = queue.pop(0)
-        # get the last node from the path
         node = path[-1]
         if node not in explored:
             neighbours = graph.getNeighbors(node)
-            # go through all neighbour nodes, construct a new path and
-            # push it into the queue
             for neighbour in neighbours:
                 new_path = list(path)
                 new_path.append(neighbour)
                 queue.append(new_path)
-                # return path if neighbour is goal
                 if neighbour == goal:
                     return new_path
  
-            # mark node as explored
             explored.append(node)
  
-    # in case there's no path between the 2 nodes
     return []
 
 print(bfs(g,38,39))
